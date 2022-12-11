0b75eec8d82fe112938d43802f9049c5eb5345c9
Sarah Roesler <sarah_roesler@yahoo.com>
2022-12-11 17:52:41 -0600

    OpenAi repo

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// enable body parser
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set Metastatic folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
